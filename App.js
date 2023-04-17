5d5a12702f82b7e8ac43287b51f3e2c8d53ff9fe
joeokat <joeokat@gmail.com>
2023-04-17 14:53:22 +0000

    setup

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Let's GO!</Text>
       <StatusBar style="auto" />
     </View>
   );
